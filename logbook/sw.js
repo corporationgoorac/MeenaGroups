a1636489095eb40501241742aa2fe73f989615f7
corporationgoorac <corporationgoorac@gmail.com>
2026-06-22 18:02:30 +0530

    Create sw.js

---
+++ logbook/sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'meena-logbook-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate Event (Cleans up old caches)
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch Event (Serves from cache if offline)
+self.addEventListener('fetch', (event) => {
+  // Ignore external API requests (like Firebase) from the service worker cache
+  if (!event.request.url.startsWith(self.location.origin)) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Return cached file or fetch from network
+      return response || fetch(event.request);
+    })
+  );
+});
